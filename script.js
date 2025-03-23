7fa77f2297ef5b0b362ab5981d94ee5586f0ab23
Reece <reece.mirv@gmail.com>
2025-03-23 18:08:26 +0900

    Implemented carousel functionality

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 const sideBar = document.getElementById('sidebar');
 const playGame = document.getElementById('play-game');
-const restartGame = document.getElementById('restart-game')
 
 // Background Constants
 const background = document.getElementById('background');
-const spriteContainer = document.getElementById('sprite-container')
+const spriteContainer = document.getElementById('sprite-container');
 
 // Sprite Constants
-const sprite1 = document.getElementById('sprite-1')
-const sprite2 = document.getElementById('sprite-2')
-const sprite3 = document.getElementById('sprite-3')
+const sprite1 = document.getElementById('sprite-1');
+const sprite2 = document.getElementById('sprite-2');
+const sprite3 = document.getElementById('sprite-3');
 
 // Settings Menu Constants 
-const settingsButton = document.getElementById('settings')
-const settingsMenu = document.getElementById('settings-menu')
-const closeSettingsMenu = document.getElementById('close-settings-btn')
+const settingsBtn = document.getElementById('settings');
+const settingsMenu = document.getElementById('settings-menu');
+const closeSettingsMenu = document.getElementById('close-settings-btn');
 
 // Volume Settings Constants
 const overlay = document.getElementById('overlay');
@@ -22,20 +21,28 @@ const volumeSlider = document.getElementById('volume-slider');
 const volumeValue = document.getElementById('volume-value');
 
 // Audio class 
-const buttonAudio = new Audio("./audio/page-flip.mp3")
-const backgroundAudio = new Audio("./audio/ominous.mp3")
-backgroundAudio.play()
+const buttonAudio = new Audio("./audio/page-flip.mp3");
+const backgroundAudio = new Audio("./audio/ominous.mp3");
+// backgroundAudio.play();
 
 // Restart Confirmation Constants
-const restartButton = document.getElementById('restart-game')
-const restartConfirmationMenu = document.getElementById('restart-confirmation')
-const yesRestart = document.getElementById('yes-restart')
-const noRestart = document.getElementById('no-restart')
+const restartBtn = document.getElementById('restart');
+const restartConfirmationMenu = document.getElementById('restart-confirmation');
+const yesRestart = document.getElementById('yes-restart');
+const noRestart = document.getElementById('no-restart');
 
 // Dialogue Constants
 const dialogueContainer = document.getElementById('dialogue-container');
-const dialogueTitle = document.getElementById('dialogue-title')
+const dialogueTitle = document.getElementById('dialogue-title');
 const dialogueText = document.getElementById("dialogue-text");
+
+// Carousel Constants
+const carousel = document.getElementById('carousel');
+const carouselItems = document.getElementById('carousel-items');
+const carouselRight = document.getElementById('carousel-right');
+const carouselLeft = document.getElementById('carousel-left');
+const inventoryBtn = document.getElementById('inventory-btn');
+const nextBtn = document.getElementById('next-btn');
 
 let typingInterval;
 let currentScene = 0;
@@ -47,10 +54,6 @@ playGame.addEventListener('click', () => {
     zoomIn();
     setScene("curr");
     sprite2.classList.toggle('show');
-
-    // Create an item and add it to the play area
-    const pointItemEnv = new PointItemEnv();
-    pointItemEnv.addItem("circle", 200, 200);
 
     // typeText("Welcome to the game! Pressing 'enter', 'right-arrow' or 'spacebar' will advance the dialogue.");
 });
@@ -97,10 +100,17 @@ document.addEventListener('keydown', (event) => {
             setScene("next");
             break;
         case "Escape":
-            sprite2.classList.toggle('show');
+            sprite2.classList.toggle("show");
+            carousel.classList.remove("show");
+            restartBtn.removeAttribute("hidden")
+
             toggleSidebar();
             toggleZoom();
-            restartButton.removeAttribute("hidden")
+
+            const itemMarkers = document.querySelectorAll(".item-marker");
+            itemMarkers.forEach((item) => {
+                item.classList.toggle("hide");
+            });
             break;
         case "ArrowLeft":
             toggleSound();
@@ -127,7 +137,8 @@ async function setScene(action) {
     // Get Scene metadata from JSON
     const response = await fetch("assets/story.json");
     const data = await response.json();
-    scenes = data["scenes"]
+    scenes = data["scenes"];
+    itemCatalogue = data["items"];
     sceneLength = Object.keys(scenes).length
 
     // Handle scene change by moving to the next or previous scene position
@@ -136,11 +147,27 @@ async function setScene(action) {
     else if (action === "prev" && currentScene != 0) currentScene -= 1;
 
     // Update the dialogue box -- present in parts if dialogue exceeds box word lmit
-    dialogueTitle.innerHTML = ' &#x2746; &nbsp; ' + scenes[`scene_${currentScene}`]['character_name'] + ' '
-    dialogue = scenes[`scene_${currentScene}`]['dialogue']
+    dialogueTitle.innerHTML = ' &#x2746; &nbsp; ' + scenes[`scene_${currentScene}`]['character_name'] + ' ';
+    dialogue = scenes[`scene_${currentScene}`]['dialogue'];
 
     // Update the background
     changeBackground(scenes[`scene_${currentScene}`]['background'])
+
+    // Update the environment play area with the background items
+    sceneItems = scenes[`scene_${currentScene}`]['items'];
+    environment = new PointItemEnv();
+
+    try {
+        // Clear any items from the previous scene
+        environment.clearItems();
+
+        // For each item in the scene object, get it from the item catalogue and add it to the play area environment
+        sceneItems.forEach(itemName => {
+            environment.addItem(itemName, itemCatalogue[itemName]);
+        });
+    } catch (error) {
+        // Ignore if there are no items in the scene
+    }
 
     // Display the character to the screen 
     sprite2.src = scenes[`scene_${currentScene}`]['character_sprite']
@@ -148,8 +175,29 @@ async function setScene(action) {
     typeText(dialogue, dialogueText);
 }
 
+// Carousel Controls
+inventoryBtn.addEventListener('click', () => {
+    const pointItemEnv = new PointItemEnv();
+    pointItemEnv.toggleCarousel();
+    pointItemEnv.createPlaceholders();
+});
+
+carouselRight.addEventListener('click', () => {
+    carouselItems.scrollBy({ left: carousel.offsetWidth, behavior: 'smooth' });
+});
+
+carouselLeft.addEventListener('click', () => {
+    carouselItems.scrollBy({ left: -carousel.offsetWidth, behavior: 'smooth' });
+});
+
+// On Screen Resize
+addEventListener("resize", (event) => {
+    const pointItemEnv = new PointItemEnv();
+    pointItemEnv.createPlaceholders();
+});
+
 // Show Restart Confirmation Menu 
-restartButton.addEventListener('click', () => {
+restartBtn.addEventListener('click', () => {
     restartConfirmationMenu.classList.add('show');
     overlay.classList.add('show');
 })
@@ -157,7 +205,7 @@ restartButton.addEventListener('click', () => {
 // Restart the game and clear progress 
 yesRestart.addEventListener('click', () => {
     playGame.innerHTML = "Start Game";
-    restartButton.setAttribute("hidden", "hidden")
+    restartBtn.setAttribute("hidden", "hidden")
     currentScene = 0;
     closeOverlays();
 });
@@ -166,7 +214,7 @@ yesRestart.addEventListener('click', () => {
 noRestart.addEventListener('click', closeOverlays)
 
 // Show settings menu
-settingsButton.addEventListener('click', () => {
+settingsBtn.addEventListener('click', () => {
     settingsMenu.classList.add('show')
     overlay.classList.add('show')
 });
@@ -209,6 +257,7 @@ function closeOverlays() {
     overlay.classList.remove('show');
     restartConfirmationMenu.classList.remove('show');
     overlay.classList.remove('show');
+    carousel.classList.remove('show');
 }
 
 // Class to generate clickable items for a scene
@@ -218,56 +267,99 @@ class PointItemEnv {
     }
 
     // Adds item to playing area, hotspot
-    addItem(name, x, y, r) {
+    addItem(name, item) {
         // Create element
-        let element = document.createElement("div");
+        const element = document.createElement("div");
         element.setAttribute("id", name)
-
-        element.style.border = "3px solid yellow";
-        element.style.borderRadius = `10px`;
-        element.style.height = "20px";
-        element.style.width = "20px";
-        element.style.zIndex = "10";
+        element.classList.add('item-marker');
 
         element.style.position = "absolute";
-        element.style.top = `${x}px`;
-        element.style.left = `${y}px`;
+        element.style.top = `${item["y"]}%`;
+        element.style.left = `${item["x"]}%`;
 
         element.addEventListener("click", () => {
-            this.retrieveItem(name);
+            // Add the item to the player's inventory & retrieve the element
+            this.inventory.push(name);
+            const itemMarker = document.getElementById(name)
+
+            // Remove the item from the screen
+            itemMarker.remove();
+
+            // Add the item to the carousel on the screen
+            const item = document.createElement('div');
+            item.setAttribute("class", "carousel-item");
+
+            const img = document.createElement('img');
+            img.src = 'character_sprites/pikachu.png';
+            item.appendChild(img);
+
+            carouselItems.appendChild(item);
+
+            // Animate the item into the carousel
+            img.style.transform = "scale(0)";
+            img.style.opacity = "0";
+            img.style.transition = "transform 1s, opacity 1s";
+
+            setTimeout(() => {
+                img.style.transform = "scale(1)";
+                img.style.opacity = "1";
+            }, 300);
+
+            // Print the inventory to the screen
+            carousel.classList.add('show');
+
+            this.createPlaceholders(item.offsetWidth);
         });
 
         document.body.appendChild(element);
     }
 
-    // Remove the item from the screen
-    removeItem(name) {
-        document.getElementById(name).style.display = "none";
+    // Create placeholder items
+    createPlaceholders(itemWidth = 100) {
+        // Clear any previously created placeholders
+        this.clearPlaceholders();
+
+        // Add carousel placeholder elements
+        const carouselWidth = carouselItems.offsetWidth;
+        const placeholderCount = Math.floor(carouselWidth / ((itemWidth + 32) - this.inventory.length));
+
+        for (let i = 0; i <= placeholderCount; i++) {
+            const itemPlaceholder = document.createElement('div');
+            itemPlaceholder.setAttribute("class", "carousel-placeholder");
+            carouselItems.appendChild(itemPlaceholder);
+        }
     }
 
-    // Give the player the item clicked on
-    retrieveItem(name) {
-        // Add the item to the player's inventory
-        this.inventory.push(name);
-
-        // Remove the item from the screen
-        this.removeItem(name);
-
-        // Print the inventory to the screen
-        this.displayInventory();
+    // Clear all the item markers in the scene
+    clearItems() {
+        const itemMarkers = document.querySelectorAll('.item-marker');
+        itemMarkers.forEach((item) => {
+            if (item.classList.contains('item-marker')) {
+                item.remove()
+            }
+        });
     }
 
-    // Print the items to the screen 
-    displayInventory() {
+    // Clear all the placeholders in the carousel
+    clearPlaceholders() {
+        const placeholders = document.querySelectorAll('.carousel-placeholder');
+        placeholders.forEach((item) => {
+            item.remove();
+        });
+    }
 
+
+    // Toggle carousel inventory
+    toggleCarousel() {
+        carousel.classList.toggle('show');
     }
 }
 
-// TODO: BOOMMMM
+// Toggle gameplay sound
 function toggleSound() {
     if (buttonAudio.paused) {
-        buttonAudio.play()
+        // buttonAudio.play();
     } else {
-        buttonAudio.currentTime = 0
+        buttonAudio.currentTime = 0;
     }
 }
